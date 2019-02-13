d08c049780e75ebe6752d48a2518806fae347f13
Anu Joshi <40773001+anujoshi222@users.noreply.github.com>
2019-02-13 03:26:37 -0800

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -37,3 +37,53 @@ function callback(response){
      FB.getLoginStatus(function (response){
         callback(response);
      });
+   }
+
+
+   function setElements(isLogged){
+     if(isLogged){
+
+
+
+       document.getElementById('profile').style.display = "block";
+       document.getElementById('fb-btn').style.display = 'none';
+       document.getElementById('logout').style.display = 'block';
+       document.getElementById('heading').style.display = 'none';
+
+     }else{
+ document.getElementById('profile').style.display = 'none';
+       document.getElementById('fb-btn').style.display = 'block';
+       document.getElementById('logout').style.display = 'none';
+       document.getElementById('heading').style.display = 'block';
+
+
+     }
+   }
+
+    function logout(){
+      FB.logout(function(response){
+        setElements(false);
+      })
+    }
+
+    function testAPI(){
+      FB.api('/me?fields=name,email,picture,birthday,location',function(response){
+        if(response && !response.error){
+buildprofile(response);
+        }
+      })
+    }
+
+    function   buildprofile(user){
+      let profile= `
+      <h3>${user.name}</h3>
+      <ul class="list-group">
+      <li class="list-group-item"><img class="rounded mx-auto rounded mx-auto d-block" src=${user.picture.data.url} alt="pic" width="150px" height="150px"></li>
+      <li class="list-group-item">User ID: ${user.id}</li>
+      <li class="list-group-item">User Email: ${user.email}</li>
+      <li class="list-group-item">Birthday: ${user.birthday}</li>
+      </ul>
+      `
+  document.getElementById('profile').innerHTML = profile;
+
+}
